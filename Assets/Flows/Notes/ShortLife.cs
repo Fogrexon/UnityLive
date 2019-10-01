4057f023d5d740e3df4fbf80527b8ee2aac79962
Fogrexon <katsu15873@gmail.com>
2019-10-01 20:51:21 +0900

    ShortLife追加

--- Assets/Flows/Notes/ShortLife.cs
+++ Assets/Flows/Notes/ShortLife.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class ShortLife : MonoBehaviour
 {
     public float LifeTime = 2.0f;
+    public Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
 
     // Update is called once per frame
     void Update()
